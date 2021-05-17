988a631ad028a23e712fd351e52f9df6660e9d68
startos <huiyu.w@foxmail.com>
2021-05-17 14:59:04 +0800

    update

--- kernel/StartOS.cpp
+++ kernel/StartOS.cpp
@@ -32,47 +32,47 @@ void operator delete[](void* p){
 
 
 
-extern "C" {
+// extern "C" {
 
-atexit_func_entry_t __atexit_funcs[ATEXIT_MAX_FUNCS];
-uarch_t __atexit_func_count = 0;
+// atexit_func_entry_t __atexit_funcs[ATEXIT_MAX_FUNCS];
+// uarch_t __atexit_func_count = 0;
 
-int __cxa_atexit(void (*f)(void *), void *objptr, void *dso){
-    if (__atexit_func_count >= ATEXIT_MAX_FUNCS) {
-        return -1;
-    }
+// int __cxa_atexit(void (*f)(void *), void *objptr, void *dso){
+//     if (__atexit_func_count >= ATEXIT_MAX_FUNCS) {
+//         return -1;
+//     }
 
-    __atexit_funcs[__atexit_func_count].destructor_func = f;
-    __atexit_funcs[__atexit_func_count].obj_ptr = objptr;
-    __atexit_funcs[__atexit_func_count].dso_handle = dso;
-    __atexit_func_count++;
+//     __atexit_funcs[__atexit_func_count].destructor_func = f;
+//     __atexit_funcs[__atexit_func_count].obj_ptr = objptr;
+//     __atexit_funcs[__atexit_func_count].dso_handle = dso;
+//     __atexit_func_count++;
 
-    return 0; /*I would prefer if functions returned 1 on success, but the ABI says...*/
-}
+//     return 0; /*I would prefer if functions returned 1 on success, but the ABI says...*/
+// }
 
-void __cxa_finalize(void *f){
-    uarch_t i = __atexit_func_count;
-    if (!f){
-        while (i--){
-            if (__atexit_funcs[i].destructor_func){
-                (*__atexit_funcs[i].destructor_func)(__atexit_funcs[i].obj_ptr);
-            }
-        }
-        return;
-    }
+// void __cxa_finalize(void *f){
+//     uarch_t i = __atexit_func_count;
+//     if (!f){
+//         while (i--){
+//             if (__atexit_funcs[i].destructor_func){
+//                 (*__atexit_funcs[i].destructor_func)(__atexit_funcs[i].obj_ptr);
+//             }
+//         }
+//         return;
+//     }
 
-    for ( ; i >= 0; --i){
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
-        if (__atexit_funcs[i].destructor_func == f){
-            (*__atexit_funcs[i].destructor_func)(__atexit_funcs[i].obj_ptr);
-            __atexit_funcs[i].destructor_func = 0;
-        }
-#pragma GCC diagnostic pop
-    }
-}
+//     for ( ; i >= 0; --i){
+// #pragma GCC diagnostic push
+// #pragma GCC diagnostic ignored "-Wpedantic"
+//         if (__atexit_funcs[i].destructor_func == f){
+//             (*__atexit_funcs[i].destructor_func)(__atexit_funcs[i].obj_ptr);
+//             __atexit_funcs[i].destructor_func = 0;
+//         }
+// #pragma GCC diagnostic pop
+//     }
+// }
 
-#define STACK_CHK_GUARD 0x595e9fbd94fda766
+// #define STACK_CHK_GUARD 0x595e9fbd94fda766
 
-uint64_t __stack_chk_guard = STACK_CHK_GUARD;
-}
+// uint64_t __stack_chk_guard = STACK_CHK_GUARD;
+// }
--- kernel/include/StartOS.hpp
+++ kernel/include/StartOS.hpp
@@ -7,21 +7,21 @@ void operator delete(void* p);
 void* operator new[](uint64_t size);
 void operator delete[](void* p);
 
-#define ATEXIT_MAX_FUNCS 32
+// #define ATEXIT_MAX_FUNCS 32
 
-extern "C" {
+// extern "C" {
 
-typedef int uarch_t;
+// typedef int uarch_t;
 
-struct atexit_func_entry_t {
-    void (*destructor_func)(void *);
-    void *obj_ptr;
-    void *dso_handle;
-};
+// struct atexit_func_entry_t {
+//     void (*destructor_func)(void *);
+//     void *obj_ptr;
+//     void *dso_handle;
+// };
 
-int __cxa_atexit(void (*f)(void *), void *objptr, void *dso);
-void __cxa_finalize(void *f);
+// int __cxa_atexit(void (*f)(void *), void *objptr, void *dso);
+// void __cxa_finalize(void *f);
 
-} // end of extern "C"
+// } // end of extern "C"
 
 #endif
