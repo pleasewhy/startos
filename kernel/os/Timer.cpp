c348fe6ffe54d6d82cff7e3e213689afad1b7c3e
pleasewhy <1943788269@qq.com>
2021-07-16 18:28:17 -0700

    modify for oscmp

--- kernel/os/Timer.cpp
+++ kernel/os/Timer.cpp
@@ -9,19 +9,21 @@
 #include "types.hpp"
 
 namespace timer {
-int ticks;
+int      ticks;
 SpinLock spinLock;
 uint32_t interval = 1000000;
-void init() {
+void     init()
+{
   ticks = 0;
   spinLock.init("timer");
 #ifdef K210
   // 不知道具体数值，选一个比较接近的
   // k210中mtime的增长频率应该是CPU频率的1/n，不清楚sipeed dock
   // 这块板子的具体数值。测试了一下好像24比较合适
-  uint32_t freq = sysctl_clock_get_freq(SYSCTL_CLOCK_CPU) / 24; // 24比较接近，误差在10ms以内
+  uint32_t freq = sysctl_clock_get_freq(SYSCTL_CLOCK_CPU) /
+                  24;  // 24比较接近，误差在10ms以内
   size_t value = INTERVAL * freq / 1000;
-  LOG_INFO("timer freq=%d value=%d\n",freq ,value);
+  LOG_INFO("timer freq=%d value=%d\n", freq, value);
   interval = value;
 #else
   // qemu中这差不多是100ms
@@ -30,14 +32,18 @@ void init() {
   setTimeout();
 }
 
-void setTimeout() {
+void setTimeout()
+{
   int t = r_time() + interval;
   sbi_set_timer(t);
 }
 
-void handleIntr() {
+void handleIntr()
+{
   spinLock.lock();
   ticks++;
+  if (ticks == 150)
+    panic("run out of time");
   wakeup(&ticks);
   spinLock.unlock();
   Task *task = myTask();
@@ -49,7 +55,8 @@ void handleIntr() {
     task->lock.lock();
     task->uticks++;
     task->lock.unlock();
-  } else {  // kernel
+  }
+  else {  // kernel
     task->lock.lock();
     task->sticks++;
     task->lock.unlock();
