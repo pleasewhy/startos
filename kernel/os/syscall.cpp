7fced8508369b1fd31c1a1fedee1f61a4d264bb6
pleasewhy <1943788269@qq.com>
2021-07-18 20:42:00 -0700

    modify for oscmp

--- kernel/os/syscall.cpp
+++ kernel/os/syscall.cpp
@@ -247,8 +247,8 @@ void syscall(void)
   num = task->trapframe->a7;
   if (num > 0 && static_cast<uint64_t>(num) < NELEM(syscalls) &&
       syscalls[num]) {
-    // printf("syscall=%d\n", num);
     task->trapframe->a0 = syscalls[num]();
+    printf("syscall=%d ret\n", num, task->trapframe->a0);
   }
   else {
     printf("\n\n");
